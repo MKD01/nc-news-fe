11a4feda7b25f54afa4a58e1f479a8591448a353
Mohammed Kabir Dastgir <mohammedkabirdastgir@MKDs-MacBook-Pro.local>
2023-07-12 13:56:17 +0100

    update nav styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import TopicsDropdown from "./TopicsDropdown";
 import SortDropdown from "./SortDropdown";
 
 const Nav = () => {
   return (
     <div id='nav-container'>
-      {/* <div id='links-container'>
-        <Link id='articles-link' to='/articles'>
-          All Articles
-        </Link>
-      </div> */}
-
-      <TopicsDropdown />
-      <SortDropdown />
+      <div id='filter-container'>
+        <TopicsDropdown />
+        <SortDropdown />
+      </div>
     </div>
   );
 };
