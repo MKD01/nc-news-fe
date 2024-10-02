6d1416bbc0c0cb3ff7794a184e23629943278c67
mkd <kabir.dastgir@hotmail.com>
2024-10-02 16:55:31 +0100

    update single article component to always start at the top

--- src/components/SingleArticles/SingleArticle.jsx
+++ src/components/SingleArticles/SingleArticle.jsx
@@ -1,7 +1,12 @@
+import { useLayoutEffect } from "react";
 import Comments from "../Comments/Comments";
 import SingleArticleCard from "./SingleArticleCard";
 
 const SingleArticle = () => {
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className='single-article-container'>
       <SingleArticleCard />
